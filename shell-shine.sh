a2cac6c424afc145833ab0567e5137d5fcfd89b2
Andre Gallo <9999415+afgallo@users.noreply.github.com>
2023-09-26 16:55:03 +1000

    fix issue with script

--- shell-shine.sh
+++ shell-shine.sh
@@ -52,12 +52,12 @@ if [ -d "$HOME/.oh-my-zsh" ]; then
 	echo "Installing Oh My Zsh plugins..."
 	# Install zsh-autosuggestions
 	if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
-		git clone "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+		git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 	fi
 
 	# Install zsh-syntax-highlighting
 	if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
-		git clone "https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+		git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 	fi
 else
 	echo "oh-my-zsh is not installed. Skipping plugin installation."
